e1877fb24281602eab88e7a5844361d27dd83f29
Crocheter <abisolaodutayo@yahoo.com>
2022-02-13 18:27:37 -0800

    first commit

---
+++ bits3.js
@@ -0,0 +1,28 @@
+const monthlyRent = 500;
+
+const yearlyRent = monthlyRent * 12;
+console.log(yearlyRent);
+
+const myName = "Abisola Odutayo";
+console.log(myName);
+
+const firstName = "Abisola";
+const lastName = "Odutayo";
+
+const sentence = "Hello " + firstName + " " + lastName + "! How are you?";
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+console.log(sentence);
+console.log(sentenceWithTemplate);
+
+const lightIsOn = false;
+
+if (lightIsOn) {
+    console.log("The light is on!");
+} else {
+    console.log("The light is _ not on!")
+}
+// always use triple =
+// context is similar to scope, 
+// topics to read again: 
+// Arrays are ordered list of anything or something, we use the square brackets, there is something called "join", another called "push"
+//
