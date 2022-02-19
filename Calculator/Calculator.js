35ccaa005c762fb254695b2656bc9c21c20b9115
Crocheter <abisolaodutayo@yahoo.com>
2022-02-19 11:15:28 -0800

    my changes

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,17 +1,18 @@
 let runningTotals = 0;
 let buffer = "0"
 let previousOperator;
-const screen = document.querySelector('.screen');
+const screen = document.querySelector(".screen");
 
 document
-.querySelector('.calc-buttons')
-.addEventListener('click', function(event) {
-    buttonClick(event.target.innerText);
-})
+    .querySelector('.calc-buttons')
+    .addEventListener('click', function(event) {
+        buttonClick(event.target.InnerText);
+});
 
 function buttonClick(value) {
+    console.log(value);
     if (isNaN(parseInt(value))) {
-
+        handleSymbol(value);
     } else {
         handleNumber(value);
     }
@@ -20,32 +21,17 @@ function buttonClick(value) {
 function handleNumber(value) {
     if (buffer === "0") {
         buffer = value;
-    } else {
-        buffer += value;
     }
-    rerender();
+    else {
+    buffer += value;
 }
+    rerender();
+    }
+
 
 function handleSymbol(value) {
-    switch (value){
-        case 'C':
-            buffer = 0;
-            runningTotals = 0;
-            break;
-        case "=":
-            if (previousOperator === null){
-                return;
-            }
-            flushOperation(parseInt(buffer));
-            previousOperator = null;
-            buffer = "" + runningTotals;
-            runningTotals = 0;
-            break;
-        case
+
+    function rerender() {
+        screen.InnerText = buffer
     }
 }
-
-function rerender() {
-    screen.innerText = buffer;
-}
-
